f75ad4580beb722802f736ef1048adf76423fef1
afoukal <ana.foukal@gmail.com>
2020-05-09 19:52:39 -0500

    5th lesson homework

--- lesson_5/homework_5_1.py
+++ lesson_5/homework_5_1.py
@@ -3,32 +3,43 @@
 # Enter your name, save it in name variable and create function print_name_three_times which returns
 # value that is equal to your name three times
 
-name_1 = None
+name_1 = 'Anastasiia'
 
 def print_name_three_times(name):
     pass
+    return name_1 * 3
 
 
 # Modify your previous program so that it will enter your name (save it in variable  name_2) and a number
 # (save in variable number) and then display their name that number of times. Each time add your name to result
 # variable
 
-name_2 = None
-number_1 = None
+name_2 = 'Anastasiia'
+number_1 = 3
 
 def print_name_number_times(number, name):
     pass
+    return name_2 * number_1
 
 
 # Enter a random string, which includes only digits. Write function sum_digits which find a sum of digits in this string
 
-string_number_1 = None
+string_number_1 = '123456789'
 
-def sum_digits(string):
+def sum_digits(string, result=0):
     pass
+    for char in string_number_1:
+       result += int(char)
+    return result
 
 
 # Create function which sums up all even numbers between 2 and 100 (include 100)
 
 def sum_even_numbers():
     pass
+    numbers = range(2,101)
+    result = 0
+    for number in numbers:
+        if number % 2 == 0:
+            result += number
+    return result
--- lesson_5/homework_5_2.py
+++ lesson_5/homework_5_2.py
@@ -3,24 +3,40 @@
 # Enter a random string in the variable string_1, then enter a character and save it in the variable char_1.
 # Write function counter, which will count how many times your character is included in your string
 
-string_1 = None
-char_1 = None
+string_1 = 'I love myself'
+char_1 = 'm'
 
-def counter(string, char):
+def counter(string, char,count=0):
     pass
+    for char in string_1:
+        if char == char_1:
+            count = count + 1
+    return count
 
 
 # Enter a random number and save it in variable number_1. Then create a function number_multiplication
 # that will multiply all the digits together and return the result.
 
-number_1 = None
-def number_multiplication(number):
+number_1 = 123
+
+def number_multiplication(number, result = 1):
     pass
+    for char in str(number_1):
+        result = result * int(char)
+    return result
+
 
 
 # Enter a random number and save it in variable number_2. Then create function number_reverse which will return
 # a number with digits of number_1 in reverse order
 
-number_2 = None
+number_2 = 896
 def number_reverse(number):
     pass
+    num_string = str(number_2)
+    index = len(num_string)
+    result = ''
+    while index > 0:
+        result += num_string[index-1]
+        index -= 1
+    return int(result)
